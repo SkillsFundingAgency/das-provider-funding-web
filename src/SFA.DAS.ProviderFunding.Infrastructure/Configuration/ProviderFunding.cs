77d5356d233afe0b9d1a4e68a2923a06e213bce9
Michael Young <mick.young1981@googlemail.com>
2022-10-28 11:53:18 +0100

    Add page content.

--- src/SFA.DAS.ProviderFunding.Infrastructure/Configuration/ProviderFunding.cs
+++ src/SFA.DAS.ProviderFunding.Infrastructure/Configuration/ProviderFunding.cs
@@ -2,8 +2,5 @@ namespace SFA.DAS.ProviderFunding.Infrastructure.Configuration
 {
     public class ProviderFunding
     {
-        public string ZendeskSectionId { get; set; }
-        public string ZendeskSnippetKey { get; set; }
-        public string ZendeskCoBrowsingSnippetKey { get; set; }
     }
 }
