4992b1bd3d72138520a369fc7c98a136eba02af1
Michael Young <mick.young1981@googlemail.com>
2022-11-01 14:04:36 +0000

    Remove config.

--- src/SFA.DAS.ProviderFunding.Web/AppStart/AddConfigurationOptionsExtension.cs
+++ src/SFA.DAS.ProviderFunding.Web/AppStart/AddConfigurationOptionsExtension.cs
@@ -10,8 +10,8 @@ namespace SFA.DAS.ProviderFunding.Web.AppStart
         public static void AddConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddOptions();
-            services.Configure<ProviderFunding.Infrastructure.Configuration.ProviderFunding>(configuration.GetSection("ProviderFunding"));
-            services.AddSingleton(cfg => cfg.GetService<IOptions<ProviderFunding.Infrastructure.Configuration.ProviderFunding>>().Value);
+            //services.Configure<ProviderFunding.Infrastructure.Configuration.ProviderFunding>(configuration.GetSection("ProviderFunding"));
+            //services.AddSingleton(cfg => cfg.GetService<IOptions<ProviderFunding.Infrastructure.Configuration.ProviderFunding>>().Value);
             services.Configure<ProviderIdams>(configuration.GetSection("ProviderIdams"));
             services.AddSingleton(cfg => cfg.GetService<IOptions<ProviderIdams>>().Value);
         }
