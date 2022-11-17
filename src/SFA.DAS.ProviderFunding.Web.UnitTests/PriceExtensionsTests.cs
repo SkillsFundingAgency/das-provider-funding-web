570138d36cb5c6c055b689af43ef756f512149a7
ruslan <ruslanss@gmail.com>
2022-11-17 16:47:59 +0000

    Html encode to pass test on azure

--- src/SFA.DAS.ProviderFunding.Web.UnitTests/PriceExtensionsTests.cs
+++ src/SFA.DAS.ProviderFunding.Web.UnitTests/PriceExtensionsTests.cs
@@ -1,14 +1,15 @@
+using System.Web;
 using SFA.DAS.ProviderFunding.Web.Extension;
 
-namespace SFA.DAS.ProviderFunding.Web.Tests
+namespace SFA.DAS.ProviderFunding.Web.UnitTests
 {
     public class PriceExtensionsTests
     {
         [Test]
         public void ToGdsMoneyAmount_FormatsDecimalValue()
         {
-            const string expected = "£1,234,567.89";
-            var actual = (1234567.89m).ToGdsMoneyFormat();
+            var expected = HttpUtility.HtmlEncode("£1,234,567.89");
+            var actual = HttpUtility.HtmlEncode((1234567.89m).ToGdsMoneyFormat());
 
             Assert.That(actual, Is.EqualTo(expected));
         }
