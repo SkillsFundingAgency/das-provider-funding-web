512e04682ac98dbf70af064d1aad7ac36b4cf4e9
Michael Young <mick.young1981@googlemail.com>
2022-11-22 11:43:34 +0000

    Add error pages

---
+++ src/SFA.DAS.ProviderFunding.Web/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Provider.Shared.UI.Attributes;
+
+namespace SFA.DAS.ProviderFunding.Web.Controllers
+{
+    [AllowAnonymous]
+    [HideNavigationBar(hideAccountHeader: false, hideNavigationLinks: true)]
+    public class ErrorController : Controller
+    {
+        [Route("error/{statuscode?}")]
+        public IActionResult Error(int? statusCode)
+        {
+            switch (statusCode)
+            {
+                case 403:
+                case 404:
+                    return View(statusCode.ToString());
+                default:
+                    return View();
+            }
+        }
+    }
+}
