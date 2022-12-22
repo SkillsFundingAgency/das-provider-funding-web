6a5d30dd52875356a16d4a46079704e6876a1cda
Jack Whiteside <Jack.Whiteside@FujitsuUKI.onmicrosoft.com>
2022-12-22 09:33:25 +0000

    FLP-72 Fixing Sonar Cube Issues

--- src/SFA.DAS.ProviderFunding.Web/Models/IndicativeEarningsReportViewModel.cs
+++ src/SFA.DAS.ProviderFunding.Web/Models/IndicativeEarningsReportViewModel.cs
@@ -2,7 +2,7 @@ namespace SFA.DAS.ProviderFunding.Web.Models;
 
 public class IndicativeEarningsReportViewModel
 {
-    public long Ukprn { get; set; }
+  public long Ukprn { get; set; }
     public decimal Total { get; set; }
     public decimal Levy { get; set; }
     public decimal NonLevy { get; set; }
--- src/SFA.DAS.ProviderFunding.Web/Services/CSVBuilder.cs
+++ src/SFA.DAS.ProviderFunding.Web/Services/CSVBuilder.cs
@@ -8,33 +8,32 @@ namespace SFA.DAS.ProviderFunding.Web.Services
     {
         public static List<AcademicYearEarningsReport> ExportToCSV(AcademicYearEarningsDto data)
         {
+            var Report = new List<AcademicYearEarningsReport>();
+
             foreach (var obj in data.Learners)
             {
-                return new List<AcademicYearEarningsReport>
+                Report.Add(new AcademicYearEarningsReport
                 {
-                    new AcademicYearEarningsReport
-                    {
 
                     FamilyName = "FamilyName",
                     GivenName = "GivenName",
                     UinqueLearningNumber = obj.Uln,
                     OnProgrammeEarnings_Jan = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 1).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 1).First().Amount : 0,
-                    OnProgrammeEarnings_Feb = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 2).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 1).First().Amount : 0,
-                    OnProgrammeEarnings_Mar = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 3).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 1).First().Amount : 0,
-                    OnProgrammeEarnings_Apr = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 4).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 1).First().Amount : 0,
-                    OnProgrammeEarnings_May = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 5).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 1).First().Amount : 0,
-                    OnProgrammeEarnings_Jun = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 6).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 1).First().Amount : 0,
-                    OnProgrammeEarnings_Jul = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 7).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 1).First().Amount : 0,
-                    OnProgrammeEarnings_Aug = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 8).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 1).First().Amount : 0,
-                    OnProgrammeEarnings_Sep = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 9).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 1).First().Amount : 0,
-                    OnProgrammeEarnings_Oct = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 10).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 1).First().Amount : 0,
-                    OnProgrammeEarnings_Nov = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 11).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 1).First().Amount : 0,
-                    OnProgrammeEarnings_Dec = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 12).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 1).First().Amount : 0,
+                    OnProgrammeEarnings_Feb = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 2).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 2).First().Amount : 0,
+                    OnProgrammeEarnings_Mar = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 3).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 3).First().Amount : 0,
+                    OnProgrammeEarnings_Apr = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 4).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 4).First().Amount : 0,
+                    OnProgrammeEarnings_May = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 5).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 5).First().Amount : 0,
+                    OnProgrammeEarnings_Jun = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 6).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 6).First().Amount : 0,
+                    OnProgrammeEarnings_Jul = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 7).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 7).First().Amount : 0,
+                    OnProgrammeEarnings_Aug = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 8).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 8).First().Amount : 0,
+                    OnProgrammeEarnings_Sep = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 9).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 9).First().Amount : 0,
+                    OnProgrammeEarnings_Oct = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 10).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 10).First().Amount : 0,
+                    OnProgrammeEarnings_Nov = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 11).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 11).First().Amount : 0,
+                    OnProgrammeEarnings_Dec = obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 12).Any() ? obj.OnProgrammeEarnings.Where(q => q.DeliveryPeriod == 12).First().Amount : 0,
                     TotalOnProgrammeEarnings = obj.TotalOnProgrammeEarnings
-                    }
-                };
+                });
             }
-            return new List<AcademicYearEarningsReport>();
+            return Report;
         }
     }
 }
